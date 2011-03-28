5292d68ceb6bc5a73082ee81699cd071eee38b7b
Jon Stearley <jrstear@sandia.gov>
2011-03-28 09:02:09 -0600

    initial commit

---
+++ bin/backfill_hourly.py
@@ -0,0 +1,90 @@
+import os,datetime
+
+# Purpose: Execute summary index searches on archived data via Splunk CLI.  This version
+#          provides the option to use the dispatch API instead of the vanilla search command.
+#          The dispatch API can be used in cases where it is simply not possible to work
+#          within the max results limit.
+
+#---------- change these variables ----------
+
+splunkSearch = "sourcetype=syslog | stats count by host | addinfo | collect index=summary marker=\"search_name=hourlycountbyhost\""
+
+startDate = "03/01/2011"
+startTime = "00:00:00"
+endDate = "03/09/2011"
+endTime = "08:00:00"
+
+intervalInMins = 60
+
+# default maxresults for CLI searches is 100
+maxResults = 50000
+
+# enable dispatch API when maxResults is simply too small
+# set maxOut as appropriate, but the default 100 should be ok
+useDispatch = True
+maxOut = 100
+
+#---------- begin script ----------
+
+# break down the start/end date and time
+
+startDateTokens = startDate.split("/")
+startMonth = int(startDateTokens[0])
+startDay = int(startDateTokens[1])
+startYear = int(startDateTokens[2])
+
+startTimeTokens = startTime.split(':')
+startHour = int(startTimeTokens[0])
+startMin = int(startTimeTokens[1])
+startSec = int(startTimeTokens[2])
+
+endDateTokens = endDate.split("/")
+endMonth = int(endDateTokens[0])
+endDay = int(endDateTokens[1])
+endYear = int(endDateTokens[2])
+
+endTimeTokens = endTime.split(':')
+endHour = int(endTimeTokens[0])
+endMin = int(endTimeTokens[1])
+endSec = int(endTimeTokens[2])
+
+# initialize start and end dates/times
+
+startDate = datetime.datetime(startYear,startMonth,startDay,startHour,startMin,startSec)
+endDate = datetime.datetime(startYear,startMonth,startDay,startHour,startMin,startSec)
+endDate += datetime.timedelta(minutes=int(intervalInMins))
+finishLineDate = datetime.datetime(endYear,endMonth,endDay,endHour,endMin,endSec)
+
+# generate and run splunk search commands via CLI
+
+i = 0
+
+while (startDate < finishLineDate):
+
+  # if near the finish line, set endDate = finishLineDate
+  if (endDate >= finishLineDate):
+    endDate = datetime.datetime(endYear,endMonth,endDay,endHour,endMin,endSec)
+
+  # convert date/time format to MM/DD/YYYY:HH:mm:ss
+  startTime = startDate.strftime("%m/%d/%Y:%H:%M:%S")
+  endTime = endDate.strftime("%m/%d/%Y:%H:%M:%S")
+
+  searchCmd = "starttime=\"" + startTime + "\" endtime=\"" + endTime + "\" " + splunkSearch
+
+  # run it!
+  if (bool(useDispatch)):
+    searchCLI = "splunk dispatch \"" + searchCmd + "\" -maxout " + str(maxOut)
+  else:
+    searchCLI = "splunk search \"" + searchCmd + "\" -maxresults " + str(maxResults)
+  print "\n\nExecuting [" + searchCLI + "]"
+  result = str.split(os.popen(searchCLI).read())
+  print result
+
+  # increment start and end dates by intervalInMins
+  startDate += datetime.timedelta(minutes=int(intervalInMins))
+  endDate += datetime.timedelta(minutes=int(intervalInMins))
+
+  # track number of searches run
+  i += 1
+
+print "Done running " + str(i) + " searches!"
---
+++ bin/expand_hostlist.py
@@ -0,0 +1,93 @@
+import csv,sys,os,hostlist,logging
+#from hostlist import expand_hostlist, collect_hostlist, numerically_sorted, Bn s.split(",")]adHostlist, __version__ as library_version
+# example usage in splunk search:
+# nnodes=16 | head 1 | lookup hostListLookup hostlist | makemv delim="," new_hostlist | mvexpand new_hostlist
+# nnodes=16 | head 1 | lookup hostListLookup short | makemv delim="," long | mvexpand long
+# to excersize forward and backward:
+# nnodes=16 | head 1 | lookup hostListLookup short | lookup hostListLookup long OUTPUTNEW short AS new_short
+
+LOG_FILENAME = '/tmp/output_from_splunk.txt'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+
+def debug(msg):
+  logging.debug(msg)
+  return
+
+# this is the forward lookup to take a list like this [1-10,15-16,18] and return
+# this: 1,2,3,4,5,6,7,8,9,10,15,16,18
+def lookup(list):
+  debug("Forward Lookup:")
+  debug(list)
+  new_list = hostlist.expand_hostlist(list)
+  # this takes the list we get back and turns it into a string for splunk
+  return ",".join(str(n) for n in new_list)
+
+# this is the reverse lookup to take this: 1,2,3,4,5,6,7,8,9,10,15,16,18
+# and return a list like this [1-10,15-16,18]
+def rlookup(list):
+  debug("Reverse Lookup:")
+  debug(list)
+  # hostlist does not like square brackets, so get rid of them
+  # this turns a string obj into a list obj
+  new_list = [str(n) for n in list.replace("[","").replace("]", "").split(",")]
+  return hostlist.collect_hostlist(new_list)
+
+def main():
+  # we can have 2 args
+  # we can tell which list comes in through splunk
+  # and based on those lists we will do a reverse or forward lookup
+  # python expand_hostlist.py long_list short_list
+  # python expand_hostlist.py short_list long_list
+  if len(sys.argv) < 3:
+    debug("Usage: python expand_hostlist.py [short|long list] [long|short list]")
+    print "Usage: python expand_hostlist.py [short|long list] [long|short list]"
+    sys.exit(0)
+
+  listf     = sys.argv[1]
+  new_listf = sys.argv[2]
+  debug(listf)
+  debug(new_listf)
+
+  # splunk passes in csv data and we take each row and do a lookup
+  rows = csv.reader(sys.stdin)
+
+  debug("got the rows")
+  i = 0
+  first = True
+  header = []
+  for line in rows:
+    if first:
+      header = line
+      if listf not in header:
+        print "List field must be in CSV data"
+        sys.exit(0)
+      csv.writer(sys.stdout).writerow(header)
+      # we return csv data for splunk too
+      w = csv.DictWriter(sys.stdout, header)
+      first = False
+
+    result = {}
+    i = 0
+    # get the header
+    while i < len(header):
+      if i < len(line):
+        result[header[i]] = line[i]
+      else:
+        result[header[i]] = ''
+      i += 1
+
+    if len(result[listf]) and len(result[new_listf]):
+      w.writerow(result)
+    elif len(result[listf]):
+      result[new_listf] = lookup(result[listf])
+      w.writerow(result)
+    elif len(result[new_listf]):
+      # reverse it ... rlookup()
+      result[listf] = rlookup(result[new_listf])
+      w.writerow(result)
+
+  debug("done with rows")
+
+debug("Starting .....")
+main()
+debug("Finished")
