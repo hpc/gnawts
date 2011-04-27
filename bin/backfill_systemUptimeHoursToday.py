1ae4c073d3f7eafbf119c7fa1f9409fd023031f9
Jon Stearley <jrstear@sandia.gov>
2011-04-27 09:17:26 -0600

    revise ras metrics stuff for nightly updating instead of only historical

--- bin/backfill_systemUptimeHoursToday.py
+++ bin/backfill_systemUptimeHoursToday.py
@@ -57,39 +57,23 @@ finishLineDate = datetime.datetime(endYear,endMonth,endDay,endHour,endMin,endSec
 
 i = 0
 
-
 while (startDate < finishLineDate):
 
   # if near the finish line, set endDate = finishLineDate
   if (endDate >= finishLineDate):
     endDate = datetime.datetime(endYear,endMonth,endDay,endHour,endMin,endSec)
 
-  # convert date/time format to MM/DD/YYYY:HH:mm:ss
-  startTime = startDate.strftime("%m/%d/%Y:%H:%M:%S")
-  endTime = endDate.strftime("%m/%d/%Y:%H:%M:%S")
-
-  # Events
-  WindowStart    = "index=summary systemStateChange=*   latest=\"" + startTime + "\" | head 1 | eval Time=strptime(\"" + startTime + "\",\"%m/%d/%Y:%H:%M:%S\") | eval Fake=1"
-  WindowEnd      = "index=summary systemStateChange=*   latest=\"" + endTime   + "\" | head 1 | eval Time=strptime(\"" + endTime   + "\",\"%m/%d/%Y:%H:%M:%S\") | eval Fake=1"
-  EventsInWindow = "index=summary systemStateChange=* earliest=\"" + startTime + "\" latest=\"" + endTime + "\" | eval Time=_time"
-  Events                = WindowStart + " | append [ search " + EventsInWindow + "] | append [ search " + WindowEnd + "] "
-
-  # Processing
-  Hours        = "sort Time | delta Time AS durationSeconds | eval systemUptimeHours=if(systemStateChange=\"USR-ERR\" AND ((crossing=\"increasing\" AND NOT Fake=1) OR (crossing=\"decreasing\" AND Fake=1)),round(durationSeconds/3600,2),0) "
-  ERRs         = "eval systemERR=if(systemStateChange=\"USR-ERR\" AND crossing=\"increasing\" AND NOT Fake=1,1,0) "
-  dayAggregate = "stats sum(systemUptimeHours) AS systemUptimeHoursToday, sum(systemERR) as systemERRCountToday "
-  Processing   = Hours + " | " + ERRs + " | " + dayAggregate 
-  # simply passing through addinfo prevents join from working in jmtti, so hack this in for now
-  FixAddinfo   = "eval info_min_time=strptime(\"" + startTime + "\",\"%m/%d/%Y:%H:%M:%S\") | eval info_max_time=strptime(\"" + endTime + "\",\"%m/%d/%Y:%H:%M:%S\") | eval _time=info_min_time"
-  # one massive hairy search!
-  searchCmd = Events + " | " + Processing + " | " + FixAddinfo + "| collect index=summary"
+  endDay = endDate.strftime("%Y-%m-%d")
+  startTime = startDate.strftime("%s")
+  endTime = endDate.strftime("%s")
+  searchCmd = "`systemUptimeYesterday("+endTime+")` | collect index=summary"
 
   # run it!
   if (bool(useDispatch)):
-    searchCLI = "splunk dispatch \'" + searchCmd + "\' -maxout " + str(maxOut)
+    searchCLI = "splunk dispatch \'" + searchCmd + "\' -maxout " + str(maxOut) + " -app hpc"
   else:
-    searchCLI = "splunk search \"" + searchCmd + "\" -maxresults " + str(maxResults)
-  print "\n\nExecuting [" + searchCLI + "]"
+    searchCLI = "splunk search \"" + searchCmd + "\" -maxresults " + str(maxResults) + " -app hpc"
+  print "\n\nendDay=" + endDay + ": Executing [" + searchCLI + "]"
   result = str.split(os.popen(searchCLI).read())
   print result
 
