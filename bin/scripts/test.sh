039d62b9d237a192f70df19f50c2da8263aab1eb
Jon Stearley <jrstear@sandia.gov>
2012-09-28 15:36:54 -0600

    prune never-quite-used ruby email stuff

--- bin/scripts/test.sh
+++
@@ -1,3 +0,0 @@
-# simple script that writes parameters 0-7 to $SPLUNK_HOME/bin/scripts/echo_output.txt 
-echo "WHAT UP!!" >> "$SPLUNK_HOME/bin/scripts/echo_output.txt" 
-echo "'$0' '$1' '$2' '$3' '$4' '$5' '$6' '$7' '$8'" >> "$SPLUNK_HOME/bin/scripts/echo_output.txt" 
